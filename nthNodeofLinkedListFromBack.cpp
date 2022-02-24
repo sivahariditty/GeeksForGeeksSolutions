cfbdc1dd6c56494efda1a2c4178b1d7e0b4c13f2
sivahariditty <sivaharivkm@gmail.com>
2022-02-24 23:00:08 +0100

    Nth node from end of linked list

---
+++ nthNodeofLinkedListFromBack.cpp
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(struct Node *head, int n)
+{
+    struct Node *ptr1 = head, *ptr2 = head;
+    for(int i = 0;i<n;i++){
+       if(ptr1 == NULL)
+           return -1;
+        ptr1 = ptr1->next;
+
+    }
+    while(ptr1 != NULL){
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    return ptr2->data;
+    
+}
+
+// { Driver Code Starts.
+
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      int n,k;
+      scanf("%d",&n);
+      scanf("%d",&k);
+      insert(n);
+      int res = getNthFromLast(start,k);
+      printf("%d\n",res);
+    }
+    return 0;
+
+}
+
+
+ void insert(int n)
+ {   int value,i;
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+
+
+
+
+  // } Driver Code Ends
