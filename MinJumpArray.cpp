1f4faf922fbbe7344e996bcf9f1736c03d82c1a5
sivahariditty <sivaharivkm@gmail.com>
2022-02-24 10:50:38 +0100

    MinJumpArray Initial

---
+++ MinJumpArray.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        int jumps = 0,i = 0;
+        while(i<n){
+            jumps++;
+            i=i+arr[i];
+        }
+        // Your code here
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
