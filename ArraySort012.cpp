1c8d82cd2867d24293ef4b4a406489b996bd6796
sivahariditty <sivaharivkm@gmail.com>
2022-02-27 10:45:36 +0100

    Sort an array of 0s, 1s and 2s initial commit

---
+++ ArraySort012.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+void sort012(int a[], int n)
+{
+    int count0 = 0,count1 =0,count2 = 0;
+    for(int i=0;i<n;i++){
+        if(a[i] == 0)
+           count0++;
+        else if(a[i] == 1)
+           count1++;
+        else
+           count2++;
+    }
+    printf("%d %d %d\n", count0,count1,count2);
+    for(int j=0;j<count0;j++){
+       a[j] = 0;
+    }
+    for(int k=count0;k<count1;k++){
+       a[k] = 1;
+    }
+    for(int l=count1;l<count2;l++){
+       a[l] = 2;
+    }
+}
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(int i=0;i<n;i++){
+            scanf("%d", &arr[i]);
+        }
+
+        sort012(arr, n);
+
+        for (int i = 0; i < n; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
